85221e8588625ca539d118945df108173dfc01c0
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-12-04 11:18:59 -0500

    Update achievements activity

--- app/src/main/java/com/wlu/aidan/supplepet/AchievementsActivity.java
+++ app/src/main/java/com/wlu/aidan/supplepet/AchievementsActivity.java
@@ -1,8 +1,17 @@
 package com.wlu.aidan.supplepet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class AchievementsActivity extends AppCompatActivity {
 
@@ -10,5 +19,35 @@ public class AchievementsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievements);
+
+        // Bottom Nav Bar Buttons
+        ImageButton homeButton = super.findViewById(R.id.home_button);
+        homeButton.setOnClickListener((view) -> {
+            print("Clicked Home Button", Toast.LENGTH_LONG);
+
+            Intent intent = new Intent(AchievementsActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        ImageButton checkButton = super.findViewById(R.id.check_button);
+        checkButton.setOnClickListener((view) -> {
+            print("Clicked Check Button",Toast.LENGTH_LONG);
+
+            Intent intent = new Intent(AchievementsActivity.this, SupplementInfoActivity.class);
+            startActivity(intent);
+        });
+
+        ImageButton bookButton = super.findViewById(R.id.book_button);
+        bookButton.setOnClickListener((view) -> {
+            print("Clicked Book Button",Toast.LENGTH_LONG);
+
+            Intent intent = new Intent(AchievementsActivity.this, SupplementLog.class);
+            startActivity(intent);
+        });
+    }
+
+    private void print(String msg, int duration){
+        Toast toast = Toast.makeText(this, msg, duration);
+        toast.show();
     }
 }
